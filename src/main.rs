adb8705439ef8c0fd7555d77de47c1adefd1cc40
terrarier2111 <hannes.gaumann@outlook.de>
2022-05-31 17:17:14 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+mod lexer;
+mod parser;
+mod diagnostics;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn test<T: Copy>(test: T) {
+
+}
+
+// https://hackernoon.com/lets-build-a-programming-language-2612349105c6
+// https://medium.com/hackernoon/compilers-and-interpreters-3e354a2e41cf
